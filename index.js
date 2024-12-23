ce6d7654d1687c07702b29ce6ab8199a0e70f594
Pegi-on <truonggiahunghgt@gmail.com>
2024-12-23 17:04:06 +0700

    Cài đặt Pug

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.set("views", "./views"); //tìm đến thư mục view
+app.set("view engine", "pug"); //sử dụng template engine pug
+
 app.get("/", (req, res) => {
-  res.send("Trang Chủ");
+  res.render("client/pages/home/index.pug");
 });
 
 app.get("/products", (req, res) => {
-  res.send("Trang Sản Phẩm");
+  res.render("client/pages/products/index.pug");
 });
 
 app.listen(port, () => {
