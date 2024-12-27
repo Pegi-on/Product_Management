695ea30dfbc375db316c4a65dcaceb5d913b9006
Pegi-on <truonggiahunghgt@gmail.com>
2024-12-27 15:48:01 +0700

    Khung giao diện sản phẩm

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT;
+
+// cấu hình route
+const routeAdmin = require("./routes/admin/index.route");
 const routeClient = require("./routes/client/index.route");
+// end cấu hình route
 
 // cấu hình database
 const databse = require("./config/database");
@@ -14,6 +18,7 @@ app.set("view engine", "pug"); //sử dụng template engine pug
 
 app.use(express.static("public")); //thiết lập thư mục chứa file tĩnh
 
+routeAdmin.index(app);
 routeClient.index(app);
 
 app.listen(port, () => {
